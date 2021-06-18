8e171b2822553477f41f0017e6dcdb54b0ac8479
Meta17Industria <laura.hernandez@cea.edu.co>
2021-06-18 08:30:16 -0500

    Ejercicios array
    trabajo 2° periodo

---
+++ index.js
@@ -0,0 +1,92 @@
+console.log("hola mundo");
+console.log('5+3');
+console.log(5+3);
+let nombre = 'a';
+console.log(nombre);
+let evaluador = true;
+console.log(evaluador)
+let vacio =null;
+console.log(vacio);
+let numero = 1;
+let numero1 =1.5;
+console.log(numero+numero1);
+const miObjeto = {
+   nombre= "Laura";
+   apellido= "Hernández Parra";
+}
+let numero2 = '5';
+console.log(miObjeto.nombre+miObjeto.apellido,numero2);
+numero2 = numero2 + numero
+console.log(numero2);
+numero2 = (20);
+console.log(numero2);
+
+// numero2=numero2 + numero;
+// // numero2=20;
+// // console.log(numero2);
+
+
+// let arreglo=[]; //Array
+
+// console.log(arreglo);
+// console.log(numero2);
+// console.log(arreglo);
+// for(let i = 0; i < 2; i++){
+//     arreglo.push(prompt('escribe tu libro favorito'));
+//     //console.log(arreglo[i]);
+// }
+// console.log(arreglo);
+
+let arr=[1, 2, 3, 4, 5];
+console.log(arr);
+arr.forEach(element => {
+    element=element*2;
+    console.log(element)
+
+//if (num==24) {
+ //   alert('aún te falta');
+//}
+
+//else{
+//    alert('no aplicas');
+//}
+
+//if (num==35) {
+//    alert('Ahí vamos');
+//    if (num < 50) {
+//        alert('Estás a tiempo');
+ //   }
+//}
+//else{
+ //       alert('te pasaste');
+//}
+
+switch (num) {
+   case 1:
+   alert('Ve al salón 24')
+   break;
+   case 2:
+       alert('Dirígete a la tesorería')
+
+   break;
+   case 3:
+       alert('Quédate en casa')
+
+   break;
+
+default:
+   alert('Ve a la información')
+
+   break;
+}
+
+let n = 0;
+while (n<10){
+alert('Te sobran '+n+ ' dolares');
+n++;
+}
+for (let index = 0; index < arr.length; index++) {
+const element = array[index];
+
+}
+console.log(arr.length);
